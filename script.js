9367246425e6c8b7c1f09c62e95baee6cfe7144a
g-soldera <gustavosevere@gmail.com>
2023-05-24 11:57:37 -0300

    title change added

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function checkColorScheme() {
   }
 }
 window.addEventListener("load", checkColorScheme)
+
+window.onload = function () {
+  var pageTitle = document.title
+  var attentionMessage = "Open Me"
+
+  document.addEventListener("visibilitychange", function (e) {
+    var isPageActive = !document.hidden
+
+    if (!isPageActive) {
+      document.title = attentionMessage
+    } else {
+      document.title = pageTitle
+    }
+  })
+}
